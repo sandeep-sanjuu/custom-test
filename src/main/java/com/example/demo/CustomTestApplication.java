f625fce738fbf74fac417c2219714ffb338604d4
sandeep-sanjuu <sandeepsanjuu3@gmail.com>
2024-06-23 11:20:21 +0530

    Second commit

--- src/main/java/com/example/demo/CustomTestApplication.java
+++ src/main/java/com/example/demo/CustomTestApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CustomTestApplication {
-
+//hi
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CustomTestApplication.class, args);
 	}
